094008487b3ed8d0243967e9bc3b69ab6ace3a54
divie2 <divine41744@gmail.com>
2023-02-21 03:52:47 -0800

    ppp

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    // Write C code here
+
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
+}
+
