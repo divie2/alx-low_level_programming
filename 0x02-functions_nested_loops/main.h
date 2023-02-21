56171c20ebd81a1edb36183cc5b8210740a64aca
divie2 <divine41744@gmail.com>
2023-02-21 04:31:45 -0800

    ppp

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: main.h
+ * Auth: Nnaemeka divine
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int putchar(char c);
