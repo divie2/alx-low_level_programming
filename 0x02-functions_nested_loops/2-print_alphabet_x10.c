195a820a2ede99c00e774168041b12be1fe9c66d
divie2 <divine41744@gmail.com>
2023-02-21 06:44:03 -0800

    ppp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,11 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
+	char letter;
 
 	while (count <= 9)
 	{
-		for (char letters = 'a'; letters <= 'z'; letters++)
+		for (letters = 'a'; letters <= 'z'; letters++)
 			putchar (letters);
 		putchar ('\n');
 		count++;
