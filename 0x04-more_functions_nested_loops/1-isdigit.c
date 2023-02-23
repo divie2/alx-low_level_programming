b1cbb5c57fa30e10af384e87ef63b1484a465c47
divie2 <divine41744@gmail.com>
2023-02-23 10:27:42 -0800

    ppp

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,9 @@
 
 /**
  * _isdigit - checks for characters that are digits
- * @c: character 
- *
- * Return: 0 or 1
+ **@c:character
+ ** Return: 0 or 1
  */
-
-
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
