f4c8511d25c10280c5c357db8ab8802a96629ba2
divie2 <divine41744@gmail.com>
2023-03-02 16:01:37 -0800

    ppp

--- 0x06-pointers_arrays_strings/divine.h
+++ 0x06-pointers_arrays_strings/divine.h
@@ -1,11 +1,7 @@
 #ifndef DIV_H
 #define DIV_H
-/*** Auth : divine
- * file: divine.h
- * Desc : Header file containing prototypes for all functions used.
-**/
- 
-int _putchar(char c); 
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,4 +9,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+#endif
